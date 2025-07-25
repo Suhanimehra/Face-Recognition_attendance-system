6590773fcdfdb9ab01a4e273f3c297dbb0351f61
Suhanimehra <118792987+Suhanimehra@users.noreply.github.com>
2025-07-25 17:20:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -282,4 +282,4 @@ if __name__ == '__main__':
     database.init_db()
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
